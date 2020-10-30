2504fb7998c2a573bb5c177bcce042112bec0a54
Texier <julestexier@live.fr>
2020-10-30 11:16:17 +0100

    SOS

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email,
-  presence: true,
-  uniqueness: true,
-  format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
-  validates :first_name , presence: true
-  validates :last_name, presence: true
-  validates :description, presence: true
+  # validates :email,
+  # presence: true,
+  # uniqueness: true,
+  # format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "email adress please" }
+  # validates :first_name , presence: true
+  # validates :last_name, presence: true
+  # validates :description, presence: true
 
 
   belongs_to :city
--- db/seeds.rb
+++ db/seeds.rb
@@ -23,7 +23,7 @@ array_tag = []
 end
 
 10.times do
-  array_user << User.create(first_name: Faker::Name.first_name , last_name: Faker::Name.last_name, description: Faker::Movies::Hobbit.quote, email: Faker::Internet.email, age: Faker::Number.within(range: 18..100), city: array_city.sample)
+  array_user << User.create(first_name: Faker::Name.first_name , last_name: Faker::Name.last_name, password: Faker::Name.last_name, description: Faker::Movies::Hobbit.quote, email: Faker::Internet.email, age: Faker::Number.within(range: 18..100), city: array_city.sample)
 end
 
 10.times do
